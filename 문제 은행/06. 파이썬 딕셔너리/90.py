03ffa811a605c3f40241e111ff9313e912d82eec
DeokhyeonKim-developer <bitworldexplorer@gmail.com>
2025-10-07 15:30:52 +0900

    '06. 파이썬 딕셔너리' 문제 풀이

---
+++ 문제 은행/06. 파이썬 딕셔너리/90.py
@@ -0,0 +1,12 @@
+icecream = {'폴라포': 1200, '빵빠레': 1800, '월드콘': 1500, '메로나': 1000}
+icecream['누가바']
+'''
+Traceback (most recent call last):
+  File "<pyshell#69>", line 1, in <module>
+    icecream['누가바']
+KeyError: '누가바'
+'''
+
+''' 에러가 발생한 이유
+딕셔너리 안에 '누가바'에 해당하는 키 값이 없어 찾을 수 없기 때문에
+'''
