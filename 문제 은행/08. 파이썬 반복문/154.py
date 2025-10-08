6c1396186adc0fbf2b6bd407d458edde5e64b486
DeokhyeonKim-developer <bitworldexplorer@gmail.com>
2025-10-08 16:53:10 +0900

    '08. 파이썬 반복문' 문제 풀이3

---
+++ 문제 은행/08. 파이썬 반복문/154.py
@@ -0,0 +1,5 @@
+리스트 = ["I", "study", "python", "language", "!"]
+
+for word in 리스트:
+    if len(word) >= 3:
+        print(word)
---
+++ 문제 은행/08. 파이썬 반복문/155.py
@@ -0,0 +1,5 @@
+리스트 = ["A", "b", "c", "D"]
+
+for letter in 리스트:
+    if letter.isupper():
+        print(letter)
