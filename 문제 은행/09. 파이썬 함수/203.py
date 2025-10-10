666cdb79426c286ff77fc840a38e00dfb752d9a4
DeokhyeonKim-developer <bitworldexplorer@gmail.com>
2025-10-10 21:44:06 +0900

    '09. 파이썬 함수' 문제 풀이

---
+++ 문제 은행/09. 파이썬 함수/203.py
@@ -0,0 +1,5 @@
+def print_coin():
+    print('비트코인')
+
+for num in range(100):
+    print_coin()
---
+++ 문제 은행/09. 파이썬 함수/204.py
@@ -0,0 +1,5 @@
+def print_coins():
+    for num in range(100):
+        print('비트코인')
+
+print_coins()
